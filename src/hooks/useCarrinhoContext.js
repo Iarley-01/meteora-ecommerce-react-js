e347922b73cdb01edeadf805d6b59ed68bf42855
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-22 17:39:56 -0300

    adicionando funções de estado do conponenete Produtos ao hook personalizado

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -3,4 +3,51 @@ import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 export const useCarrinhoContext = () => {
   const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+  
+  function adicionarProduto(novoProduto) {
+    const temOProduto = carrinho.some((itemDoCarrinho) => {
+      itemDoCarrinho.id === novoProduto.id
+    });
+    
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto
+      ]);
+    }
+    
+    setCarrinho((carrinhoAnterior) => carrinhoAnterior.map((itemDoCarrinho) => {
+      if (itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
+      return itemDoCarrinho;
+
+      
+    }))
+  }
+  
+  function removerProduto(idProduto) {
+      const estaNoCarrinho = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === idProduto);
+      
+      if (!estaNoCarrinho) {
+        return;
+      }
+      
+      if (estaNoCarrinho.quantidade === 1) {
+        setCarrinho(carrinhoAnterior => carrinhoAnterior.filter(itemDoCarrinho => itemDoCarrinho.id === idProduto));
+      } else {
+        setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
+          if (itemDoCarrinho.id === idProduto) {
+            itemDoCarrinho.quantidade -= 1;
+          }
+          return itemDoCarrinho;
+        }));
+     }
+   }
+   
+   return {
+    carrinho,
+    setCarrinho,
+    adicionarProduto,
+    removerProduto
+   }
 }
