c6de89b1924369812f585c666012950acf8ceb0c
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-25 13:22:18 -0300

    criando as funções para utilizar o reducer

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -3,6 +3,21 @@ import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 import { ADD_PRODUTO, REMOVE_PRODUTO, UPDATE_QUANTIDADE } from "@/reducers/carrinhoReducer";
 
+const addProdutoAction = (novoProduto) => ({
+  type: ADD_PRODUTO,
+  payload: novoProduto,
+});
+
+const removeProdutoAction = (produtoId) => ({
+  type: REMOVE_PRODUTO,
+  payload: produtoId,
+});
+
+const updateQuantidadeAction = (produtoId, quantidade) => ({
+  type: UPDATE_QUANTIDADE,
+  payload: { produtoId, quantidade },
+});
+
 export const useCarrinhoContext = () => {
   const { carrinho,
   dispatch,
